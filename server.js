baeec5002f7ce5f79c3bed53ceed0a9a89875e03
Shahid <abdul.baset@aci-bd.com>
2019-11-19 17:51:03 +0600

    express tutorial

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const shopRouter = require('./routes/shop');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
